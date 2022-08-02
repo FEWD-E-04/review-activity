c2f47fd148652f99b63e53185141bd7f5d903449
Laize Ferraz <laizeferraz@gmail.com>
2022-08-02 20:25:14 +1000

    adding solutions

--- script.js
+++ script.js
@@ -4,9 +4,54 @@
  */
 
 // Selectors
+const list = document.querySelector('#list');
+const btn = document.getElementById('button');
+const headline = document.getElementById('headline');
+const listItems = document.querySelectorAll('#list');
+let newItemCounter = 1;
 
 // Iterate the ul items
-
+for(const item of listItems) {
+item.addEventListener('click', function(event) {
+    if(event.target.nodeName == 'LI') {
+        headline.innerHTML = event.target.innerHTML
+        const list = event.target.parentNode.children
+        for (let i = 0; i < list.length; i++) {
+            list[i].classList.remove('bg-warning')
+        }
+        event.target.classList.add('bg-warning')
+    }
+})
+}
 // Event Listener - Click the item change the heading
 
 // Event Listener - Click the button add a new item
+
+btn.addEventListener('click', function(event) {
+    list.innerHTML += `<li class="border-bottom m-3"> Something new ${newItemCounter} </li>`
+    newItemCounter++
+})
+
+/*Another solution */
+
+// for(i = 0; i < listItems.length; i++) {
+//   listItems[i].addEventListener('click', activateItem);
+// }
+
+// function activateItem (event) {
+//   if(event.target.nodeName == 'LI') {
+//     headline.innerHTML = event.target.innerHTML;
+//     const list = event.target.parentNode.children;
+//     for (i = 0; i < list.length; i++){
+//       list[i].classList.remove('bg-warning');
+//     }
+//     event.target.classList.add('bg-warning');
+//   } 
+// }
+
+// btn.addEventListener('click', createNewItem);
+
+// function createNewItem() {
+//   list.innerHTML += `<li class="border-bottom m-3"> Something new ${newItemCounter} </li>`
+//   newItemCounter++
+// }
